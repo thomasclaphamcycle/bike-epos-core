b54d0f7e96134eb703d43caf2e1f42a7a5e9d4b9
Thomas Witherspoon <thomas@claphamcycle.com>
2026-03-27 22:27:21 +0000

    Add workshop status strips to day snapshot

--- frontend/src/features/workshop/WorkshopJobOverlay.tsx
+++ frontend/src/features/workshop/WorkshopJobOverlay.tsx
@@ -8,6 +8,7 @@ import {
   workshopRawStatusActionClass,
   workshopRawStatusClass,
   workshopRawStatusLabel,
+  workshopRawStatusSurfaceClass,
 } from "./status";
 
 type WorkshopJobOverlayLine = {
@@ -2886,7 +2887,7 @@ export const WorkshopJobOverlay = ({
                               return (
                                 <article
                                   key={job.id}
-                                  className={`workshop-os-job-workspace-section__list-item${isCurrentJob ? " workshop-os-schedule-day-snapshot__item--current" : ""}`}
+                                  className={`workshop-os-job-workspace-section__list-item ${workshopRawStatusSurfaceClass(job.rawStatus)}${isCurrentJob ? " workshop-os-schedule-day-snapshot__item--current" : ""}`}
                                 >
                                   <div className="workshop-os-schedule-day-snapshot__item-row">
                                     <div>
